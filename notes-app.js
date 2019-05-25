971019b751b41605462f5ec878e96766e989f4fa
eyoelyil <eyoelyil22@gmail.com>
2019-05-25 14:13:57 +0300

    update

--- notes-app.js
+++ notes-app.js
@@ -45,7 +45,6 @@ document.querySelector('#create-note').addEventListener('click', function (e) {
 document.querySelector('#search-text').addEventListener('input', function (e) {
   filters.searchText = e.target.value
   renderNotes(notes, filters)
-
 })
 
 document.querySelector('#filter-by').addEventListener('change', function (e) {
