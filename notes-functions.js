cbdb4568a435c922e80ead25833fd64c4b99a57b
eyoelyil <eyoelyil22@gmail.com>
2019-05-27 01:05:11 +0300

    update

--- notes-functions.js
+++ notes-functions.js
@@ -1,1 +1,10 @@
-console.log(filters)
+// Read existing notes from localStorage
+const getSavedNotes = function () {
+    const notesJSON = localStorage.getItem('notes')
+
+    if (notesJSON !== null) {
+        return JSON.parse(notesJSON)
+    } else {
+        return []
+    }
+}
