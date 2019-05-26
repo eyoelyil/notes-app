bc75c5412ec8ba02edeeface9cf416af876768f6
eyoelyil <eyoelyil22@gmail.com>
2019-05-27 01:52:26 +0300

    updat

--- notes-functions.js
+++ notes-functions.js
@@ -9,4 +9,34 @@ const getSavedNotes = function () {
     }
 }
 
-//update
+// Save the notes to localStorage
+const saveNotes = function (notes) {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+// Generate the DOM structure for a note
+const generateNoteDOM = function (note) {
+    const noteEl = document.createElement('p')
+
+    if (note.title.length > 0) {
+        noteEl.textContent = note.title
+    } else {
+        noteEl.textContent = 'Unnamed note'
+    }
+
+    return noteEl
+}
+
+// Render application notes
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(function (note) {
+        const noteEl = generateNoteDOM(note)
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
