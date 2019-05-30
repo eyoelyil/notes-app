6fe55fe86edb53d4a98abd81270ed75d7ced0c96
eyoelyil <eyoelyil22@gmail.com>
2019-05-30 21:19:30 +0300

    updat

--- notes-functions.js
+++ notes-functions.js
@@ -1,3 +1,5 @@
+console.log(uuidv4())
+
 // Read existing notes from localStorage
 const getSavedNotes = function () {
     const notesJSON = localStorage.getItem('notes')
@@ -14,6 +16,17 @@ const saveNotes = function (notes) {
     localStorage.setItem('notes', JSON.stringify(notes))
 }
 
+// Remove a note from the list
+const removeNote = function (id) {
+    const noteIndex = notes.findIndex(function (note) {
+        return note.id === id
+    })
+
+    if (noteIndex > -1) {
+        notes.splice(noteIndex, 1)
+    }
+}
+
 // Generate the DOM structure for a note
 const generateNoteDOM = function (note) {
     const noteEl = document.createElement('div')
@@ -23,6 +36,11 @@ const generateNoteDOM = function (note) {
     // Setup the remove note button
     button.textContent = 'x'
     noteEl.appendChild(button)
+    button.addEventListener('click', function () {
+        removeNote(note.id)
+        saveNotes(notes)
+        renderNotes(notes, filters)
+    })
 
     // Setup the note title text 
     if (note.title.length > 0) {
